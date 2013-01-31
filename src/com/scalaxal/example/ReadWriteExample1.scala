a471839ebb882c63f5be6b88186014964e2cbe94
workingDog <kodefarmer@gmail.com>
2013-01-31 13:56:16 +1100

    added XalPrintWriter and XalToXml to start the writing process. Also added ReadWriteExample1

---
+++ src/com/scalaxal/example/ReadWriteExample1.scala
@@ -0,0 +1,24 @@
+package com.scalaxal.example
+
+import com.scalaxal.io.{XalPrintWriter, XalFileReader, XalFromXml}
+import xml.PrettyPrinter
+
+/**
+ * Author: Ringo Wathelet
+ * Date: 31/01/13 
+ * Version: 1
+ */
+
+object ReadWriteExample1 {
+  def main(args: Array[String]) {
+    println("....XAL ReadWriteExample1 start...\n")
+
+    val xal = new XalFileReader().getXalFromFile("./xal-files/XAL.XML")
+
+//    println("xal = " + xal + "\n")
+
+    if (xal.isDefined) new XalPrintWriter().write(xal, new PrettyPrinter(80, 3))
+
+    println("\n....XAL ReadWriteExample1 done...")
+  }
+}

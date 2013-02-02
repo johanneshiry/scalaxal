ba12b1811522b05d3e2e4982fe67beff281c980f
workingDog <kodefarmer@gmail.com>
2013-02-02 13:08:34 +1100

    working on XalToXmlNext, a more generic way to output to xml

--- src/com/scalaxal/io/XalToXmlNext.scala
+++ src/com/scalaxal/io/XalToXmlNext.scala
@@ -1,11 +1,45 @@
-package com.scalaxal.io
+/*
+ * Copyright (c) 2013, Ringo Wathelet
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * - Neither the name of "scalaxal" nor the names of its contributors may
+ *   be used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
+package com.scalaxal.io
 
 import xml._
 import scala.Predef._
 import scala.Some
 import com.scalaxal.xAL.Content
 
+/**
+ * @author Ringo Wathelet
+ * Date: 01/02/13
+ * Version: 1
+ */
 
 case class TestInner(inner: Option[String] = None,
                      inType: Double,
@@ -17,8 +51,10 @@ case class TestAddress(outer: Option[String] = None,
                        addressLatitude: Option[Content],
                        outObject: Option[TestInner])
 
+/** Factory to convert XAL object to scala xml NodeSeq */
+object XalToXmlNext extends XmlExtractor {
 
-object XalToXmlNext {
+  def getXmlFrom[A: XalToXml](xal: A) = XalToXmlNext.toXml(xal)
 
   // todo TypeOccurrence, attributes, DependentThoroughfares, RangeType, NumberType, NumberOccurrence
 
